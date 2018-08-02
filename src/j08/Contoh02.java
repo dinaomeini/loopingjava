3c9bed569a6263d821a71fb8cd54852d166c43f5
Fahdina <Fahdina@LAPTOP-DUGV0GBD>
2018-08-02 12:07:43 -0700

    Bilangan 1-10

---
+++ src/j08/Contoh02.java
@@ -0,0 +1,41 @@
+package j08;
+import java.util.Scanner;
+public class Contoh02 {
+    public static void main(String[] args)
+    {
+        int x;
+        
+        for(x=1; x <= 10; x++)
+        {
+            System.out.print(x + " ");
+            
+        }
+        
+        x = 1;
+        
+        while(x <= 10)
+        {    
+            System.out.print(x + " ");
+            x++;
+            
+        }
+    
+        System.out.println();
+    
+        x = 1;
+                
+        do
+        { 
+            System.out.print(x + " ");
+            x++;
+        } while(x <= 10);
+        
+    }
+}    
+            
+            
+        
+            
+         
+    
+
